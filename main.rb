bd4c3fad3fe4c7f6d39ce245d516498ae68fd471
Fickry Iman <fickry.iman@gmail.com>
2023-09-11 20:44:21 +0700

    linters fixes #1

--- main.rb
+++ main.rb
@@ -8,24 +8,12 @@ def start
     print "\nSelected Menu: #{user_input}\n"
 
     case user_input
-    when 1
-      puts 'user chooses 1. List All Books'
-    when 2
-      puts 'user chooses 2. List All Music Albums'
-    when 3
-      puts 'user chooses 3. List All Games'
-    when 4
-      puts "user chooses 4. List All Books Label (e.g. 'Gift', 'New')"
-    when 5
-      puts "user chooses 5. List All Music Genres (e.g 'Comedy', 'Thriller')"
-    when 6
-      puts "user chooses 6. List All Games Authors (e.g. 'Stephen King')"
-    when 7
-      puts 'user chooses 7. Add a Book'
-    when 8
-      puts 'user chooses 8. Add a Music Album'
-    when 9
-      puts 'user chooses 9. Add a Game'
+    when 1, 4, 7
+      book_menu(user_input)
+    when 2, 5, 8
+      music_album_menu(user_input)
+    when 3, 6, 9
+      game_menu(user_input)
     when 0
       puts 'user chooses 0. Quit / Exit from App\n'
       puts
@@ -46,6 +34,39 @@ def options_menu
   puts '8. Add a Music Album'
   puts '9. Add a Game'
   puts '0. Quit / Exit from App'
+end
+
+def book_menu(user_input)
+  case user_input
+  when 1
+    puts 'user chooses 1. List All Books'
+  when 4
+    puts "user chooses 4. List All Books Label (e.g. 'Gift', 'New')"
+  when 7
+    puts 'user chooses 7. Add a Book'
+  end
+end
+
+def music_album_menu(user_input)
+  case user_input
+  when 2
+    puts 'user chooses 2. List All Music Albums'
+  when 5
+    puts "user chooses 5. List All Music Genres (e.g 'Comedy', 'Thriller')"
+  when 8
+    puts 'user chooses 8. Add a Music Album'
+  end
+end
+
+def game_menu(user_input)
+  case user_input
+  when 3
+    puts 'user chooses 3. List All Games'
+  when 6
+    puts "user chooses 6. List All Games Authors (e.g. 'Stephen King')"
+  when 9
+    puts 'user chooses 9. Add a Game'
+  end
 end
 
 start
