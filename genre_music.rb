530967906b3cb1900cb9764b90dfd5b97b16ba11
rbd3 <rabedesanaa@gmail.com>
2023-09-12 10:41:27 +0300

    Fix linters error

--- genre_music.rb
+++ genre_music.rb
@@ -1,15 +1,15 @@
 class GenreMusic
-    attr_accessor :items, :name
-    attr_reader :id
+  attr_accessor :items, :name
+  attr_reader :id
 
-    def initialize(id, name, items)
-        @id = id
-        @name = name
-        @items = items
-    end
+  def initialize(id, name, items)
+    @id = id
+    @name = name
+    @items = items
+  end
 
-    def add_item(item)
-       @items << item
-       item.genre = self
-    end
+  def add_item(item)
+    @items << item
+    item.genre = self
+  end
 end
