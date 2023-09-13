119502e8c872f0928720ecc2013d119e8bd219ea
SAZZAD1Q2 <kurulia115@gmail.com>
2023-09-13 10:12:31 +0600

    gameSchema.sql created

---
+++ gameSchema.sql
@@ -0,0 +1,27 @@
+-- Create the 'items' table to store common properties of all items
+CREATE TABLE items (
+    id SERIAL PRIMARY KEY,
+    label VARCHAR(100),
+    genre VARCHAR(100),
+    author VARCHAR(100),
+    publish_date DATE,
+    archived BOOLEAN
+);
+
+CREATE TABLE games (
+    item_id INT PRIMARY KEY REFERENCES items(id),
+    last_played_at TIMESTAMP
+);
+
+CREATE TABLE authors (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100)
+);
+
+
+ALTER TABLE items
+ADD COLUMN author_id INT,
+ADD CONSTRAINT fk_author_id
+FOREIGN KEY (author_id)
+REFERENCES authors(id);
