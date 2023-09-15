ee9da3ea311fe0b0ea5d9084b6277a3465dbed17
Fickry Iman <fickry.iman@gmail.com>
2023-09-15 08:13:28 +0700

    join all schema in this schema

--- schema.sql
+++ schema.sql
@@ -34,3 +34,68 @@ CREATE TABLE IF NOT EXISTS public.labels
 TABLESPACE pg_default;
 ALTER TABLE IF EXISTS public.labels
     OWNER to postgres;
+
+
+/* Database schema to keep the structure of the MusicAlbum database*/
+
+CREATE TABLE music_album (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    label VARCHAR(100),
+    genre VARCHAR(100),
+    author VARCHAR(100),
+    publish_date DATE,
+    archived BOOLEAN,
+    publisher VARCHAR(100),
+    cover_state VARCHAR(100),
+    on_spotify BOOLEAN,
+    weight_kg FLOAT,
+    PRIMARY KEY(id)
+);
+
+
+CREATE TABLE genre_music (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100),
+    PRIMARY KEY(id)
+);
+
+-- Create a foreign key to associate GenreMusic with MusicAlbums
+ALTER TABLE music_album
+ADD COLUMN genre_music_id INT,
+ADD CONSTRAINT fk_genre_music_id
+FOREIGN KEY (genre_music_id)
+REFERENCES public.genre_music(id);
+
+
+-- Create the 'items' table to store common properties of all items
+CREATE TABLE items (
+    id SERIAL PRIMARY KEY,
+    label VARCHAR(100),
+    genre VARCHAR(100),
+    author VARCHAR(100),
+    publish_date DATE,
+    archived BOOLEAN
+);
+
+CREATE TABLE games (
+    item_id INT PRIMARY KEY REFERENCES items(id),
+    label VARCHAR(100),
+    genre VARCHAR(100),
+    author VARCHAR(100),
+    publish_date DATE,
+    archived BOOLEAN,
+    last_played_at TIMESTAMP
+);
+
+CREATE TABLE authors (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100)
+);
+
+
+ALTER TABLE items
+ADD COLUMN author_id INT,
+ADD CONSTRAINT fk_author_id
+FOREIGN KEY (author_id)
+REFERENCES authors(id);
